dc8bcf7f3677346a64e29a829318d797c7829292
Will Hou <maize@ezi.am>
2011-08-15 01:26:58 -0400

    initial commit

---
+++ src/com/maize/CardMagic/AppBrain.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2010 Maize Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.maize.CardMagic;
+
+import java.util.HashSet;
+
+import android.hardware.SensorEvent;
+import android.util.Log;
+
+/**
+ * AppBrain.java - The Model of the card swapper. Monitors states of the card.
+ * 
+ */
+public class AppBrain 
+{
+	/* Value and states */
+	private static final int THRESHOLD = 5; 	// default 5cm
+	private boolean processing = false;
+	
+	/*
+	 * The reason for using enum instead of boolean for monitoring states is that 
+	 * boolean only supports a max of 2 states. But with enum, more states can be 
+	 * supported and added easily in the future. (i.e. more cards can be used) 
+	 */
+	private enum State { HEARTS, CLUBS };		// There are only 2 cards in this app, King of Hearts and King of Clubs
+	private State state = State.HEARTS;			// Default card is King of Hearts
+	
+	
+	/* Listeners */
+	protected HashSet<BrainStateChangeListener> listeners;	
+	
+	
+	/* Constructor */
+	public AppBrain()
+	{
+		listeners = new HashSet<BrainStateChangeListener>();
+	}
+	
+	
+	/**
+	 * Takes in a SensorEvent (Sensor.TYPE_PROXIMITY), analyzes it and broadcasts the appropriate card drawable id to listeners.
+	 * @param event
+	 */
+	public void changeState(SensorEvent event)
+	{
+		if (!processing) // Avoid rapid changes due to spam of SensorEvents
+		{
+			processing = true;
+			
+			float[] valuesArray = event.values;
+
+			// According to the Android docs, value of distance measured is stored in element 0 of the float array
+			// Sensor.TYPE_PROXIMITY: values[0]: Proximity sensor distance measured in centimeters
+			float value = valuesArray[0];
+			
+			Log.i("SensorChanged", "Distance is  " + value + "cm");
+			
+			// Swap card when distance detected by the proximity sensor is closer than the threshold
+			if (value < THRESHOLD) 
+			{
+				if (state == State.HEARTS)
+				{
+					fireStateChange(R.drawable.king_clubs);
+					state = State.CLUBS;
+				}
+				else
+				{
+					fireStateChange(R.drawable.king_hearts);
+					state = State.HEARTS;
+				}
+			}
+				
+			processing = false;
+		}			
+	}
+
+	// Broadcasts the resource id of the desired card to all the listeners
+	protected void fireStateChange(int resId)
+	{
+		for (BrainStateChangeListener l : listeners) 
+		{
+			l.swapCard(resId);
+		}
+	}
+	
+	
+	public void addBrainStateChangeListener(BrainStateChangeListener l)
+	{
+		if (l != null) 
+		{
+			listeners.add(l);
+		}
+	}
+	
+	
+	public void removeBrainStateChangeListener(BrainStateChangeListener l)
+	{
+		listeners.remove(l);
+	}
+	
+	
+	/* Listener */
+	public interface BrainStateChangeListener
+	{
+		public void swapCard(int resId);
+	}
+}
---
+++ src/com/maize/CardMagic/Main.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2010 Maize Labs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.maize.CardMagic;
+
+import com.maize.CardMagic.AppBrain.BrainStateChangeListener;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+/**
+ * Main.java - The main activity of the app
+ * 
+ */
+public class Main extends Activity 
+{
+	/* UI components */
+	private ImageView cardView;
+	
+	
+	/* App model */
+	AppBrain brain;
+	
+	
+	/* Options menu constant */
+	private final static int DEMO = Menu.FIRST; 
+	private final static int ABOUT = Menu.FIRST + 1; 
+	
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        cardView = (ImageView) findViewById(R.id.card);
+        
+        brain = new AppBrain();
+        brain.addBrainStateChangeListener(new BrainStateChangeListener() 
+        {			
+			public void swapCard(int card) 
+			{
+				cardView.setImageResource(card);
+			}
+		});
+        
+        // Get instance of SensorManager
+        SensorManager manager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        
+        // Checks if the device has a light sensor before proceeding
+        if (manager.getSensorList(Sensor.TYPE_PROXIMITY).size() == 0)
+        {
+        	// OMG, the device has no proximity sensor!!!
+        	UIUtils.showSensorMissingDialog(this);
+        }
+        else
+        {
+        	// Get the proximity sensor
+        	Sensor sensor = manager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+            
+            // Create a listener that listens to the light sensor's event change
+            manager.registerListener(new SensorEventListener() 
+            {			
+    			// Called when sensor values have changed.
+    			public void onSensorChanged(SensorEvent event) 
+    			{
+    				brain.changeState(event);
+    			}
+
+            	// Called when the accuracy of a sensor has changed. (We don't need this)
+    			public void onAccuracyChanged(Sensor sensor, int accuracy) 
+    			{
+    				Log.i("AccuracyChanged", "Accuracy is " + accuracy);
+    			}
+    			
+    		}, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+        }        
+    }
+    
+    
+    /* Options menu */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+    	super.onCreateOptionsMenu(menu);
+    	MenuItem item;
+    	item = menu.add(0, DEMO, 0, "Demo");
+    	item.setIcon(android.R.drawable.ic_menu_help);
+    	item = menu.add(0, ABOUT, 0, "About");
+    	item.setIcon(android.R.drawable.ic_menu_info_details);
+        return true;    	
+    }
+    
+    
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item)
+    {
+    	Intent intent;
+    	switch (item.getItemId())
+    	{
+    		case DEMO:
+    			// Load up the demo Youtube video
+				intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=DT5K96iwoas"));
+				startActivity(intent);
+    			return true;
+    			
+    		case ABOUT:
+    			intent = new Intent(this, About.class);
+    			startActivity(intent);
+    			return true;
+    	}
+    	return false;
+    }
+}
