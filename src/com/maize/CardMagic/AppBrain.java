fa5aa1dcb54cb84a16c18a6fc0db07a720059c74
Will Hou <maize@ezi.am>
2011-10-02 21:03:45 -0400

    Update apache license year

--- src/com/maize/CardMagic/AppBrain.java
+++ src/com/maize/CardMagic/AppBrain.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 Maize Labs
+ * Copyright 2011 Maize Labs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
